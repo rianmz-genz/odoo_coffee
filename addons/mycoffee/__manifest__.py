80a4f6cac62235dd43a370cba4820935d0607d1c
rianmz-genz <adrianji1945@gmail.com>
2024-08-10 12:17:38 +0700

    fix

--- addons/mycoffee/__manifest__.py
+++ addons/mycoffee/__manifest__.py
@@ -18,13 +18,14 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'calendar', 'sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/inherit_calendar.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ addons/mycoffee/models/inherit_calendar.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+import logging
+
+logger = logging.getLogger(__name__)
+
+class CalendarInherit(models.Model):
+    _inherit = 'calendar.event'
+    
+    sale_ids = fields.Many2many('sale.order', 'calendar_sale_rel', 'calendar_id', 'sale_id', string='Sales Orders')
+    fleet_ids = fields.Many2many('fleet.vehicle', 'calendar_fleet_rel', 'calendar_id', 'fleet_id', string='Fleets')
+
+class SaleInherit(models.Model):
+    _inherit = 'sale.order'
+    
+    calendar_ids = fields.Many2many('calendar.event', 'calendar_sale_rel', 'sale_id', 'calendar_id', string='Calendars')
+    fleet_ids = fields.Many2many('fleet.vehicle', 'sale_fleet_rel', 'sale_id', 'fleet_id', string='Fleets')
+
+class FleetInherit(models.Model):
+    _inherit = 'fleet.vehicle'
+    
+    calendar_ids = fields.Many2many('calendar.event', 'calendar_fleet_rel', 'fleet_id', 'calendar_id', string='Calendars')
+    sale_ids = fields.Many2many('sale.order', 'sale_fleet_rel', 'fleet_id', 'sale_id', string='Sales Orders')
